ea66c84521c425aa219f3c7d8e8dbee5cb25eb3c
Mathews <mathewsrodrigues9@gmail.com>
2023-04-13 15:27:45 -0300

    test: :white_check_mark: Testando classe Ingredients

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("bacon")
+    ingredient2 = Ingredient("manteiga")
+
+    assert hash(ingredient1) == hash(ingredient1)
+    assert hash(ingredient1) != hash(ingredient2)
+
+    assert ingredient1 == ingredient1
+    assert ingredient1 != ingredient2
+
+    assert repr(ingredient1) == "Ingredient('bacon')"
+
+    assert ingredient1.name == "bacon"
+
+    assert ingredient1.restrictions == {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.ANIMAL_MEAT,
+    }
